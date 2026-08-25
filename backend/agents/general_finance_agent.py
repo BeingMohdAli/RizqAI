b1e4776d887f8b4c42b1a3c52be686bcdf3b91c3
BeingMohdAli <mohdalisaad868@gmail.com>
2026-08-25 19:40:03 +0530

    Added finance agent

--- backend/agents/general_finance_agent.py
+++ backend/agents/general_finance_agent.py
@@ -1,4 +1,4 @@
-from config import llm
+from config import guardrail_llm as general_finance_llm
 from graph.state import GraphState
 from prompts.general_finance_prompt import GENERAL_FINANCE_PROMPT
 from schemas.general_finance_state import GeneralFinanceAnswer
@@ -14,13 +14,13 @@ async def general_finance_agent(state: GraphState) -> GraphState:
     print("General Finance agent Working....")
 
     try:
-        general_llm = llm.with_structured_output(GeneralFinanceAnswer)
+        structured_llm  = general_finance_llm.with_structured_output(GeneralFinanceAnswer)
         prompt = ChatPromptTemplate.from_messages([
             ("system", GENERAL_FINANCE_PROMPT),
             ("human", "{user_query}"),
         ])
         messages = await prompt.ainvoke({"user_query": state.user_query})
-        answer = await general_llm.ainvoke(messages)
+        answer = await structured_llm.ainvoke(messages)
 
     except Exception as e:
         return {
