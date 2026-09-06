680cb5471943d966467ccb511320f7ca7eb0d002
Gaohar Imran <gaoharimran29@gmail.com>
2026-09-06 11:31:32 +0530

    Changed fallback models and their providers

--- backend/config.py
+++ backend/config.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 
 from dotenv import load_dotenv
-from langchain_mistralai import ChatMistralAI
 from langchain_groq import ChatGroq
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openrouter import ChatOpenRouter
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,9 +13,9 @@ load_dotenv(BASE_DIR / ".env")
 
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 
 
 primary_llm = ChatGoogleGenerativeAI(
@@ -26,19 +26,19 @@ primary_llm = ChatGoogleGenerativeAI(
 )
 
 
-fallback_llm_1 = ChatMistralAI(
-    model="mistral-large-latest",
-    temperature=0.2,
-    timeout=30,
-    api_key=MISTRAL_API_KEY
-)
-
-
-fallback_llm_2 = ChatGroq(
-    model="openai/gpt-oss-20b",
+fallback_llm_1 = ChatGroq(
+    model="openai/gpt-oss-120b",
     temperature=0.2,
     timeout=30,
     api_key=GROQ_API_KEY
+)
+
+
+fallback_llm_2 = ChatOpenRouter(
+    model="openai/gpt-oss-120b",
+    temperature=0.2,
+    timeout=30,
+    api_key=OPENROUTER_API_KEY
 )
 
 
@@ -47,5 +47,7 @@ llm = primary_llm.with_fallbacks([fallback_llm_1, fallback_llm_2])
 
 guardrail_llm = ChatGroq(
     model="openai/gpt-oss-20b",
-    api_key=GROQ_API_KEY
+    api_key=GROQ_API_KEY,
+    timeout=30,
+    temperature=0
 )
